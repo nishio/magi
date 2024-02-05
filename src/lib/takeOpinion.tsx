20db72495fca77c7e4f361d57608703ed5e22e37
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2024-02-05 18:09:32 +0900

    fix UpperCase/LowerCase problem

--- src/lib/takeOpinion.tsx
+++ src/lib/takeOpinion.tsx
@@ -1,11 +1,12 @@
 export const takeOpinion = (text: string) => {
+  text = text.toUpperCase();
   if (text.startsWith("STRONG YES")) {
     return { v: 2, text: "STRONG YES" };
-  } else if (text.startsWith("Yes")) {
+  } else if (text.startsWith("YES")) {
     return { v: 1, text: "Yes" };
-  } else if (text.startsWith("Neutral")) {
+  } else if (text.startsWith("NEUTRAL")) {
     return { v: 0, text: "Neutral" };
-  } else if (text.startsWith("No")) {
+  } else if (text.startsWith("NO")) {
     return { v: -1, text: "No" };
   } else if (text.startsWith("STRONG NO")) {
     return { v: -2, text: "STRONG NO" };
