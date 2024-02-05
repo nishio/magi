16782f18798fe8aa81cc8a1b98bb125e9a8d8061
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2024-02-05 15:29:48 +0900

    refactor

---
+++ src/components/DiscussionView.tsx
@@ -0,0 +1,41 @@
+import { Discussion } from "../lib/data";
+import { takeOpinion } from "../lib/takeOpinion";
+import { YesNo } from "./YesNo";
+import { Consensus } from "./Consensus";
+import { BackToTop } from "./BackToTop";
+
+export const DiscussionView = ({ discussion }: { discussion: Discussion }) => {
+  return (
+    <>
+      {" "}
+      <div className="mt-2 p-4 bg-slate-700 rounded-lg shadow-lg">
+        <span className="block mb-1 text-sm text-gray-400 font-semibold">
+          Discussion Topic:
+        </span>
+        <p className="text-white font-medium">{discussion.topic}</p>
+      </div>
+      <div className="grid grid-cols-3 gap-4 mt-4">
+        {discussion.viewpoints.map((viewpoint, index) => (
+          <div key={index} className="text-center">
+            <a href={`#detail-${index + 1}`} className="hover:underline">
+              <p className="text-lg md:text-xl">{viewpoint.name}</p>
+            </a>
+            <YesNo v={takeOpinion(viewpoint.text).v} />
+          </div>
+        ))}
+      </div>
+      <Consensus discussion={discussion} />
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {discussion.viewpoints.map((viewpoint, index) => (
+          <div key={index} className="mt-8">
+            <h2 className="text-2xl font-semibold" id={`detail-${index + 1}`}>
+              {viewpoint.name}
+            </h2>
+            <p className="text-gray-400">{viewpoint.text}</p>
+            <BackToTop />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
--- src/pages/d/[id].tsx
+++ src/pages/d/[id].tsx
@@ -6,10 +6,8 @@ import { GetStaticProps, GetStaticPaths } from "next";
 
 import "../../app/globals.css";
 import { takeOpinion } from "../../lib/takeOpinion";
-import { YesNo } from "../../components/YesNo";
-import { Consensus } from "../../components/Consensus";
-import { BackToTop } from "../../components/BackToTop";
 import { Navigation } from "../../components/Navigation";
+import { DiscussionView } from "../../components/DiscussionView";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = discussions.map((discussion) => ({
@@ -34,50 +32,16 @@ const DiscussionPage = ({ discussion }: { discussion: Discussion }) => {
   return (
     <>
       <Head>
-        <title>Plural Viewpoints - {discussion.topic}</title>
+        <title>Plural Viewpoints - {content}</title>
         <meta name="description" content={content} />
-        <meta
-          property="og:title"
-          content={`Plural Viewpoints - ${discussion.topic}`}
-        />
+        <meta property="og:title" content={`Plural Viewpoints - ${content}`} />
         <meta property="og:description" content={content} />
         <meta property="og:type" content="article" />
       </Head>
       <div className="bg-slate-800 text-white min-h-screen">
         <div className="md:mx-4 md:my-4">
           <Navigation />
-          <div className="mt-2 p-4 bg-slate-700 rounded-lg shadow-lg">
-            <span className="block mb-1 text-sm text-gray-400 font-semibold">
-              Discussion Topic:
-            </span>
-            <p className="text-white font-medium">{discussion.topic}</p>
-          </div>
-          <div className="grid grid-cols-3 gap-4 mt-4">
-            {discussion.viewpoints.map((viewpoint, index) => (
-              <div key={index} className="text-center">
-                <a href={`#detail-${index + 1}`} className="hover:underline">
-                  <p className="text-lg md:text-xl">{viewpoint.name}</p>
-                </a>
-                <YesNo v={takeOpinion(viewpoint.text).v} />
-              </div>
-            ))}
-          </div>
-          <Consensus discussion={discussion} />
-
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-            {discussion.viewpoints.map((viewpoint, index) => (
-              <div key={index} className="mt-8">
-                <h2
-                  className="text-2xl font-semibold"
-                  id={`detail-${index + 1}`}
-                >
-                  {viewpoint.name}
-                </h2>
-                <p className="text-gray-400">{viewpoint.text}</p>
-                <BackToTop />
-              </div>
-            ))}
-          </div>
+          <DiscussionView discussion={discussion} />
           <hr className="border-gray-400" />
           <Navigation />
         </div>
