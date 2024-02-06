9eb9c0e9c23f880acabed2ceb3e3831d5a52e02c
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2024-02-06 15:30:13 +0900

    client-side calling

--- src/pages/add_topic.tsx
+++ src/pages/add_topic.tsx
@@ -13,20 +13,59 @@ import {
 } from "firebase/firestore";
 import Link from "next/link";
 import { initializeApp } from "firebase/app";
+import { prompts } from "@/lib/prompts";
+
+// const model = "gpt-3.5-turbo"; // for local rapid test
+const model = "gpt-4";
+
+const call_gpt = async (system: string, prompt: string, key: string) => {
+  const url = "https://api.openai.com/v1/chat/completions";
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${key}`,
+  };
+  const data = {
+    model,
+    messages: [
+      {
+        role: "system",
+        content: system,
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+  };
+  console.log("before fetch");
+  const response = await fetch(url, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(data),
+  });
+  const responseData = await response.json();
+  console.log("after fetch");
+  // console.log(responseData);
+  return responseData.choices[0].message.content;
+};
 
 // make spinner component
-const Spinner = () => {
-  return (
-    <div className="spinner-border" role="status">
-      <span>Generating...</span>
-    </div>
-  );
+const Spinner = ({ state }: { state: string }) => {
+  if (state === "running") {
+    return <span className="animate-spin">🤔</span>;
+  } else if (state === "done") {
+    return <span>✅</span>;
+  } else {
+    return null;
+  }
 };
 
 export default function AddTopic() {
   const [result, setResult] = React.useState("");
   const [key, setKey] = React.useState("");
-  const [isLoading, setIsLoading] = React.useState(false);
+  const [state1, setState1] = React.useState("");
+  const [state2, setState2] = React.useState("");
+  const [state3, setState3] = React.useState("");
   const [url, setUrl] = React.useState("");
 
   useEffect(() => {
@@ -34,22 +73,47 @@ export default function AddTopic() {
   }, []);
 
   const submitTopic = () => {
-    setIsLoading(true);
+    setState1("running");
+    setState2("running");
+    setState3("running");
 
     const elem = document.getElementById("topic")! as HTMLInputElement;
     const topic = elem.value;
-    fetch("/api/get_answer", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ q: topic, key }),
-    }).then((res) => {
-      console.log(res);
-      res.json().then(({ response }) => {
-        setResult(response);
-        setIsLoading(false);
-      });
+
+    Promise.all([
+      call_gpt(prompts[0], topic, key).then((response) => {
+        setState1("done");
+        return response;
+      }),
+      call_gpt(prompts[1], topic, key).then((response) => {
+        setState2("done");
+        return response;
+      }),
+      call_gpt(prompts[2], topic, key).then((response) => {
+        setState3("done");
+        return response;
+      }),
+    ]).then((results) => {
+      setResult(
+        JSON.stringify({
+          id: "",
+          topic,
+          viewpoints: [
+            {
+              name: "Synthetic Technocracy",
+              text: results[0],
+            },
+            {
+              name: "Corporate Libertarianism",
+              text: results[1],
+            },
+            {
+              name: "Digital Democracy",
+              text: results[2],
+            },
+          ],
+        })
+      );
     });
   };
 
@@ -62,7 +126,7 @@ export default function AddTopic() {
     const docRef = await addDoc(collection(firestore, "topics"), obj);
 
     console.log("Document written with ID: ", docRef.id);
-    const shareableUrl = `/t/${docRef.id}`;
+    const shareableUrl = `https://magi-nishio.vercel.app/t/${docRef.id}`;
     setUrl(shareableUrl);
   };
   return (
@@ -130,7 +194,9 @@ export default function AddTopic() {
             >
               Submit Topic
             </button>
-            {isLoading && <Spinner />}
+            <Spinner state={state1} />
+            <Spinner state={state2} />
+            <Spinner state={state3} />
           </div>
           <p className="mt-4">
             It calls GPT-4 three times to generate answer from three different
@@ -166,14 +232,9 @@ export default function AddTopic() {
             className="w-full p-2 mt-2 bg-gray-900"
             type="text"
             placeholder="URL to share"
-            defaultValue={url && window.location.origin + url}
+            defaultValue={url}
           />
-          {url && (
-            <Link href={url}>
-              {window.location.origin}
-              {url}
-            </Link>
-          )}
+          {url && <Link href={url}>{url}</Link>}
         </div>
       </div>
     </div>
