96131f07ef3f69a87a72a24b7eafd44916bd3434
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2024-02-14 17:36:20 +0900

    fix for Twitter

--- src/pages/d/[id].tsx
+++ src/pages/d/[id].tsx
@@ -39,7 +39,10 @@ const DiscussionPage = ({ discussion }: { discussion: Discussion }) => {
         <meta property="og:title" content={title} />
         <meta property="og:description" content={content} />
         <meta property="og:type" content="article" />
-        <meta property="og:image" content={`/api/ogp/${discussion.id}`} />
+        <meta
+          property="og:image"
+          content={`https://magi-nishio.vercel.app/api/ogp/${discussion.id}`}
+        />
       </Head>
       <div className="bg-slate-800 text-white min-h-screen">
         <div className="md:mx-4 md:my-4">
